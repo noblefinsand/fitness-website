72e0a744156f5f0aaf2b1add3895fe65a97747a0
Noble Finsand <thefinsandman@gmail.com>
2019-12-02 18:17:56 -0700

    added modal content and javascript to open and close the modal

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("contact").addEventListener("click", function() {
+  document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+  document.querySelector(".bg-modal").style.display = "none";
+});
